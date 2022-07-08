0d131576e5e3b02103ad4e563a69e9e3f5940ce4
Carmen Chiu <carmenchiu@Carmens-iMac.local>
2022-07-08 13:07:32 -0700

    first commit

---
+++ Playlist/Playlist/PlaylistApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaylistApp.swift
+//  Playlist
+//
+//  Created by Carmen Chiu on 7/8/22.
+//
+
+import SwiftUI
+
+@main
+struct PlaylistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
